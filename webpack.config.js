094f1edc17ab507ce2e686c284946572355e41e2
Andres Viera <andyviera212@hotmail.com>
2020-08-10 21:14:34 -0500

    Commit webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebPackPlugin       = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin() ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
+    ]
+
+}
+
